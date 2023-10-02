71ced1d4e45cd2da42995ac96628543ce541910b
Laith007 <laithalmajaly007@gmail.com>
2023-10-02 19:40:50 +0300

    Changes Made

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const userName = prompt("Enter your name:");
-const gender = prompt("Enter your gender: Male / Female");
+let gender;
+
+while (
+  gender != "Male" &&
+  gender != "Female" &&
+  gender != "male" &&
+  gender != "female"
+) {
+  gender = prompt("Enter your gender: Male / Female");
+}
 
 if (gender === "Male" || gender === "male") {
   alert(`Mr.${userName}, Welcome to our caffe! 🥰`);
 } else if (gender === "Female" || gender === "female") {
   alert(`Ms.${userName}, Welcome to our caffe! 🥰`);
-} else {
-  alert(`${userName}, Welcome to our caffe! 🥰`);
 }
 
 const drink = prompt("Do you want a Hot Or Cold drink?");
@@ -14,3 +21,8 @@ const drinkName = prompt("Enter the name of the drink:");
 
 alert("Your Drink Is Being Prepared! 😀");
 console.log(`${userName} ordered ${drinkName}`);
+
+const userInfo = [userName, gender, drink, drinkName];
+for (let i = 0; i < userInfo.length; i++) {
+  console.log(userInfo[i]);
+}
