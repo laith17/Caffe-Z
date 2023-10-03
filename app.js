d855f3cc419b7121a447d4000a2da2e3509c8812
laith17 <laithalmajaly007@gmail.com>
2023-10-03 23:25:32 +0300

    Adding some events

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-const userName = prompt("Enter your name:");
-let gender;
+// const userName = document.getElementById("userName").value;
+// const age = document.getElementById("age").value;
+// const drink = document.getElementsByClassName("drinkType").value;
+// const drinkName = document.getElementById("drinkName").value;
+// const submit = document.getElementById("submit");
 
-function genderPrompt() {
-  while (
-    gender != "Male" &&
-    gender != "Female" &&
-    gender != "male" &&
-    gender != "female"
-  ) {
-    gender = prompt("Enter your gender: Male / Female");
-  }
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+submit.addEventListener("click", () => {
+  const userName = document.getElementById("userName").value;
+  const age = document.getElementById("age").value;
+  const listArray = [];
+  let drink = document.querySelectorAll("input[type='checkbox']");
+  drink.forEach((item) => {
+    if (item.checked) {
+      listArray.push(item.value);
+    }
+  });
+  drink = listArray.join("/");
+  const drinkName = document.getElementById("drinkName").value;
 
-  if (gender === "Male" || gender === "male") {
-    alert(`Mr.${userName}, Welcome to our caffe! 🥰`);
-  } else if (gender === "Female" || gender === "female") {
-    alert(`Ms.${userName}, Welcome to our caffe! 🥰`);
-  }
-}
-genderPrompt();
+  let division = document.createElement("div");
+  let paragraph = document.createElement("p");
+  let list = document.createElement("ul");
+  let listItem1 = document.createElement("li");
+  let listItem2 = document.createElement("li");
+  let listItem3 = document.createElement("li");
 
-const drink = prompt("Do you want a Hot Or Cold drink?");
-const drinkName = prompt("Enter the name of the drink:");
+  paragraph.innerHTML = `Order name: ${userName}`;
+  listItem1.innerHTML = `Your age is: ${age}`;
+  listItem2.innerHTML = `Drink type: ${drink}`;
+  listItem3.innerHTML = `Drink Name: ${drinkName}`;
 
-alert("Your Drink Is Being Prepared! 😀");
-console.log(`${userName} ordered ${drinkName}`);
+  division.appendChild(paragraph);
+  division.appendChild(list);
+  list.appendChild(listItem1);
+  list.appendChild(listItem2);
+  list.appendChild(listItem3);
 
-const userInfo = [userName, gender, drink, drinkName];
-for (let i = 0; i < userInfo.length; i++) {
-  console.log(userInfo[i]);
-}
+  document.body.appendChild(division);
+});
 
-let division = document.createElement("div");
-let paragraph = document.createElement("p");
-let list = document.createElement("ul");
-let listItem1 = document.createElement("li");
-let listItem2 = document.createElement("li");
-let listItem3 = document.createElement("li");
+// console.log(`${userName} ordered ${drinkName}`);
 
-paragraph.innerHTML = `Order name: ${userName}`;
-listItem1.innerHTML = `Gender: ${gender}`;
-listItem2.innerHTML = `Drink type: ${drink}`;
-listItem3.innerHTML = `Drink Name: ${drinkName}`;
-
-division.appendChild(paragraph);
-division.appendChild(list);
-list.appendChild(listItem1);
-list.appendChild(listItem2);
-list.appendChild(listItem3);
-
-document.body.appendChild(division);
+// const userInfo = [userName, gender, drink, drinkName];
+// for (let i = 0; i < userInfo.length; i++) {
+//   console.log(userInfo[i]);
+// }
